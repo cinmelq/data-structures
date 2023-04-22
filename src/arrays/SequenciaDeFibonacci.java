ddeaef2cd0e19739b4da785204e6ac96c5e1cd2c
cinmelq <cinthiaxes@gmail.com>
2023-04-22 15:55:40 -0300

    using recursiveness to generate fibonacci

--- src/arrays/SequenciaDeFibonacci.java
+++ src/arrays/SequenciaDeFibonacci.java
@@ -4,16 +4,29 @@ public class SequenciaDeFibonacci {
 	public static void main(String[] args) {
 
 		final int[] FIBONACCI = new int[20]; // declara e cria um array com 20 posições.
-		//ignora a posição 0 pois na sequencia de fibonacci não existe 0.
+		// ignora a posição 0 pois na sequencia de fibonacci não existe 0.
 		FIBONACCI[1] = 1;
 		FIBONACCI[2] = 2;
 
-		for (int i = 3; i < 20; i++) {
-			FIBONACCI[i] = FIBONACCI[i - 1] + FIBONACCI[i - 2];
-		}
-		for (int i = 1; i < FIBONACCI.length; i++) {
-			System.out.println(FIBONACCI[i]);
+		SequenciaDeFibonacci fibonacci = new SequenciaDeFibonacci();
+		fibonacci.populateFibonacci(FIBONACCI, 3);
+
+		fibonacci.printArray(FIBONACCI, 1);
+	}
+
+	void populateFibonacci(int[] array, int count) {
+		if (count < array.length) {
+			array[count] = array[count - 1] + array[count - 2];
+			count++;
+			populateFibonacci(array, count);
 		}
 	}
 
+	void printArray(int[] array, int count) {
+		if (count < array.length) {
+			System.out.println(array[count]);
+			count++;
+			printArray(array, count);
+		}
+	}
 }
